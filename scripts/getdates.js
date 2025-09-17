8d083f19c765209c0f4b868505e76b3dd88b7c37
muyolim <dmukonda@byupathway.edu>
2025-09-17 22:04:18 +0200

    Add updated WDD131 home page files for audit

--- scripts/getdates.js
+++
@@ -1,5 +0,0 @@
-// Set current year
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-// Set last modified date
-document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
